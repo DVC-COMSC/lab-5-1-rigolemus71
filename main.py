189c19392ecc74e1f7c5caa1077aac576de440bd
rigolemus71 <rigolemus71@gmail.com>
2024-07-03 00:06:56 -0700

    Final Update

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 def getinput():
-   v1 = int(input("Please enter an integer: "))
-   v2 = int(input("Please enter a second integer: "))
-
+   num = int(input('Enter a number: '))
+   return num
 
 def getsum(v1, v2):
     total = v1 + v2
-
+    return total
 
 def printval(v1, v2, total):
     print(v1, v2, total)
-
 
 def main():
     userval1 = getinput()
     userval2 = getinput()
     total = getsum(userval1, userval2)
     printval(userval1, userval2, total)
-
 
 if __name__ == '__main__':
     main()
